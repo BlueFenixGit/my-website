e584a342e85fa804fd53101db47f089a1e8e69fa
BlueFenixGit <bluefenixgithub@protonmail.com>
2024-07-20 15:32:17 +1000

    Update website content

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById('myButton').addEventListener('click', function() {
     alert('Button clicked!');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Page loaded');
+});
